c011a4a94405431b225d52e9defc9fe574eb6b35
NesarAhmedRazon <grim.roy@gmail.com>
2025-09-19 11:29:38 +0600

    :speech_balloon: Order Status.php transfered

---
+++ smdp-courier.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Plugin Name: SMDP Courier
+ * Plugin URI: https://github.com/NesarAhmedRazon/SMDP-Courier
+ * Description: A plugin for adding product types.
+ * Version: 0.0.1
+ * Author: Nesar Ahmed
+ * Author URI: https://nesarahmed.dev/
+ * License: GPLv2 or later
+ * Text Domain: smdp-courier
+ * Domain Path: /languages/
+ */
+
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+
+if (!defined('SMDP_COURIER_DOMAIN')) {
+    define('SMDP_COURIER_DOMAIN', 'smdp-courier');
+}
+
+if (!defined('SMDP_COURIER_DIR')) {
+    define('SMDP_COURIER_DIR', plugin_dir_path(__FILE__));
+}
+
+if (!defined('SMDP_COURIER_URL')) {
+    define('SMDP_COURIER_URL', plugin_dir_url(__FILE__));
+}
+
+if (!defined('SMDP_COURIER_FILE')) {
+    define('SMDP_COURIER_FILE', __FILE__);
+}
+
+add_action('woocommerce_init', 'smdpc_wooReady');
+function smdpc_wooReady()
+{
+    include_once('inc/order-status.php');
+}
